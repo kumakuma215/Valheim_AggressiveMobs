ddd957e8945c5094dea4ddb2d6e4e76be1fb0e74
skhalifa <samuel.khalifa@etu.u-bordeaux.fr>
2022-02-17 18:42:13 +0100

    first commit

---
+++ AgressiveMobs/AggressiveMobs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AggressiveMobs
+{
+    [BepInPlugin("kumakuma215.AggressiveMobs", "Aggressive mobs", "1.0.0")]
+    [BepInProcess("valheim.exe")]
+    public class AggressiveMobs : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("kumakuma215.AggressiveMobs");
+
+        void Awake()
+        {
+            harmony.PatchAll();
+            
+        }
+
+        [HarmonyPatch(typeof(BaseAI), nameof(BaseAI.IsEnemy))]
+        class Aggro_Patch
+        {
+            static bool Prefix(Character a, Character b, ref bool __result)
+            {
+                if(a != b) {
+                    __result = a.m_name != b.m_name;
+                    return false;
+                }
+
+                return true;
+            }
+        }
+    }
+}
